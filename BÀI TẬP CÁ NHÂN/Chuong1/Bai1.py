a55865db516c64f452872549cd26e25ea27ae8cd
Hiang24 <nguyenhoang240804@gmail.com>
2025-01-12 17:50:50 +0700

    Day code 12/112025

--- BÀI TẬP CÁ NHÂN/Chuong1/Bai1.py
+++ BÀI TẬP CÁ NHÂN/Chuong1/Bai1.py
@@ -1,9 +1,7 @@
-a=int(input("a="))
-b=int(input("b="))
-c=int(input("c="))
-max=a
-if(max<b):
-    max=b
-if(max<c):
-    max=c     
-print("So lon nhat: ", max)        
+gia_niem_yet = int(input('Nhap gia niem yet: '))
+chiet_khau = int(input('Nhap chiet khau: '))
+
+vat = (gia_niem_yet - chiet_khau)*0.01
+gia_ban = gia_niem_yet - chiet_khau + vat
+
+print(f'Gia ban: {gia_ban}')
